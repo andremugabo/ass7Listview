f0d6f8433d254d888c68a73a627f2a4389ed9538
andremugabo <andremugabo@yahoo.fr>
2023-07-01 00:43:25 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  RefreshControl,
+} from "react-native";
+import { users as initialData } from "./data";
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState(initialData.slice(3));
+  const [refreshing, setRefreshing] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const [hasMoreData, setHasMoreData] = useState(true);
+  const [slidesToPull, setSlidesToPull] = useState(3);
+
+  const loadMoreData = () => {
+    if (isLoadingMore || !hasMoreData) {
+      return;
+    }
+
+    setIsLoadingMore(true);
+
+    // Simulate an API call to fetch more data
+    setTimeout(() => {
+      setSlidesToPull(data.length < slidesToPull ? slidesToPull + 3 : 0);
+      const newData = data.slice(slidesToPull);
+
+      setData([...data, ...newData]);
+      setIsLoadingMore(false);
+      setHasMoreData(false); // For this example, assume we've reached the end of data
+    }, 2000);
+  };
+
+  const onRefresh = () => {
+    if (refreshing) {
+      return;
+    }
+
+    setRefreshing(true);
+
+    // Simulate an API call to fetch refreshed data
+    setTimeout(() => {
+      setData(data.slice(3));
+      setRefreshing(false);
+    }, 2000);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, marginTop: 50 }}>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        onEndReached={loadMoreData}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={() => {
+          if (!isLoadingMore) return null;
+
+          return (
+            <View style={{ paddingVertical: 16 }}>
+              <Text style={{ textAlign: "center" }}>Loading more data...</Text>
+            </View>
+          );
+        }}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={{
+              padding: 16,
+              borderBottomWidth: 1,
+              borderBottomColor: "#ccc",
+            }}
+          >
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
